fd00194a8764abdb31631ea342da0f4f6ea29337
Tero Marttila <tero.marttila@kontena.io>
2018-05-22 10:55:28 +0300

    vagrant dev

---
+++ vagrant/provision-ubuntu.sh
@@ -0,0 +1,21 @@
+set -uex
+
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/docker.gpg
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install -y docker-ce
+
+adduser vagrant docker
+
+apt-get install unattended-upgrades
