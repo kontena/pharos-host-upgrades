4b0c1210925256540f620e6b3829ffc5faedb2e9
Tero Marttila <tero.marttila@kontena.io>
2018-05-22 11:36:30 +0300

    systemd/exec: refactor, start with fail mode to avoid concurrent jobs

--- systemd/exec.go
+++ systemd/exec.go
@@ -9,58 +9,110 @@ import (
 )
 
 type systemdExec struct {
-	conn *dbus.Conn
+	unit      string
+	cmd       []string
+	conn      *dbus.Conn
+	ch        chan string
+	jobStatus string
 }
 
-func Exec(name string, cmd []string) error {
-	var unit = fmt.Sprintf("%v.service", name)
-	var se systemdExec
-	var ch = make(chan string)
-	var properties = []dbus.Property{
-		dbus.PropExecStart(cmd, false), // XXX: bool flag meaning is inverted?
-		dbus.PropType("oneshot"),
-	}
-
-	log.Printf("systemd/exec %v: cmd=%v", unit, cmd)
-
+func (se *systemdExec) connect() error {
 	if conn, err := dbus.NewSystemConnection(); err != nil {
 		return fmt.Errorf("dbus.NewSystemConnection: %v", err)
 	} else {
-		defer conn.Close()
-
 		se.conn = conn
 	}
 
-	if err := se.conn.ResetFailedUnit(unit); err == nil {
+	return nil
+}
+
+func (se *systemdExec) reset() error {
+	log.Printf("systemd/exec %v: reset", se.unit)
+
+	if err := se.conn.ResetFailedUnit(se.unit); err == nil {
 
 	} else if dbusErr, ok := err.(godbus.Error); ok && dbusErr.Name == "org.freedesktop.systemd1.NoSuchUnit" {
-		// ignore
+		return nil
 	} else {
-		return fmt.Errorf("dbus.ResetFailedUnit: %v", err)
+		return fmt.Errorf("dbus.ResetFailedUnit %v: %v", se.unit, err)
 	}
 
-	if _, err := se.conn.StartTransientUnit(unit, "replace", properties, ch); err != nil {
-		return fmt.Errorf("dbus.StartTransientUnit: %v", err)
+	return nil
+}
+
+// fails if unit is already running
+func (se *systemdExec) start() error {
+	var properties = []dbus.Property{
+		dbus.PropExecStart(se.cmd, false), // XXX: bool flag meaning is inverted?
+		dbus.PropType("oneshot"),
 	}
 
-	log.Printf("systemd/exec %v: started...", unit)
+	log.Printf("systemd/exec %v: start %#v", se.unit, properties)
 
-	if status := <-ch; status != "done" {
-		return fmt.Errorf("Job status: %v", status)
+	if _, err := se.conn.StartTransientUnit(se.unit, "fail", properties, se.ch); err != nil {
+		return fmt.Errorf("dbus.StartTransientUnit %v: %v", se.unit, err)
 	}
 
-	log.Printf("systemd/exec %v: done", unit)
+	return nil
+}
 
-	/*
-		if properties, err := se.conn.GetUnitTypeProperties("test.service", "Service"); err != nil {
-			return fmt.Errorf("dbus.GetUnitProperties: %v", err)
-		} else {
-			log.Printf("Unit properties:")
-			for name, value := range properties {
-				log.Printf("\t%v: %v", name, value)
-			}
+func (se *systemdExec) wait() error {
+	log.Printf("systemd/exec %v: wait", se.unit)
+
+	se.jobStatus = <-se.ch
+
+	return nil
+}
+
+func (se *systemdExec) show() error {
+	if properties, err := se.conn.GetUnitTypeProperties(se.unit, "Service"); err != nil {
+		return fmt.Errorf("dbus.GetUnitProperties %v: %v", se.unit, err)
+	} else {
+		log.Printf("Unit properties:")
+		for name, value := range properties {
+			log.Printf("\t%v: %v", name, value)
 		}
-	*/
+	}
+
+	return nil
+}
+
+func (se *systemdExec) close() {
+	se.conn.Close()
+}
+
+func Exec(name string, cmd []string) error {
+	var se = systemdExec{
+		unit: fmt.Sprintf("%v.service", name),
+		cmd:  cmd,
+		ch:   make(chan string),
+	}
+
+	log.Printf("systemd/exec %v: cmd=%v", se.unit, cmd)
+
+	if err := se.connect(); err != nil {
+		return err
+	} else {
+		defer se.close()
+	}
+
+	if err := se.reset(); err != nil {
+		return err
+	}
+
+	if err := se.start(); err != nil {
+		return err
+	}
+
+	if err := se.wait(); err != nil {
+		return err
+	}
+
+	if se.jobStatus == "done" {
+		log.Printf("systemd/exec %v: done", se.unit)
+	} else {
+		return fmt.Errorf("Job %v", se.jobStatus)
+	}
 
 	return nil
 }
