1b6a146439939a4fc3188dff96c5055e3088d1e9
Tero Marttila <tero.marttila@kontena.io>
2018-06-18 15:57:07 +0300

    fix kubectl drain to use --delete-local-data

--- kubectl/drain.go
+++ kubectl/drain.go
@@ -1,5 +1,5 @@
 package kubectl
 
 func Drain(node string) error {
-	return kubectl("drain", "--ignore-daemonsets", "--force", node)
+	return kubectl("drain", "--delete-local-data", "--ignore-daemonsets", "--force", node)
 }
